ce7ca6ff1e9a4e047fbcdfaa4eb4282c4ad2f6c0
Shuvo <nahidhasan2809@gmail.com>
2019-03-12 08:17:17 +0600

    User activity is created

---
+++ app/src/main/java/com/example/medicare/UserActivity.java
@@ -0,0 +1,54 @@
+package com.example.medicare;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class UserActivity extends AppCompatActivity {
+    private Button button2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+        button2 = (Button) findViewById(R.id.dcButton);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent dcIntent = new Intent(getApplicationContext(),FindDoctor.class);
+                startActivity(dcIntent);
+            }
+        });
+        button2 = (Button) findViewById(R.id.MButton);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent dcIntent = new Intent(getApplicationContext(),FindMedicine.class);
+                startActivity(dcIntent);
+            }
+        });
+        button2 = (Button) findViewById(R.id.blButton);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent dcIntent = new Intent(getApplicationContext(),BloodBank.class);
+                startActivity(dcIntent);
+            }
+        });
+        button2 = (Button) findViewById(R.id.amButton);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent dcIntent = new Intent(getApplicationContext(),CallAmbulance.class);
+                startActivity(dcIntent);
+            }
+        });
+    }
+}

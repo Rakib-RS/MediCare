a0bf5542d5d67e628a0d5182ffb45fadd0848c6c
Shuvo <nahidhasan2809@gmail.com>
2019-03-12 08:21:04 +0600

    find medicine activity is created.

---
+++ app/src/main/java/com/example/medicare/FindMedicine.java
@@ -0,0 +1,79 @@
+package com.example.medicare;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class FindMedicine extends AppCompatActivity {
+    private Button button2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_medicine);
+        button2 = (Button) findViewById(R.id.rajButton);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent dcIntent = new Intent(getApplicationContext(),RajshahiActivity.class);
+                startActivity(dcIntent);
+            }
+        });
+        button2 = (Button) findViewById(R.id.chiButton);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent dcIntent = new Intent(getApplicationContext(),RajshahiActivity.class);
+                startActivity(dcIntent);
+            }
+        });
+        button2 = (Button) findViewById(R.id.DhButton);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent dcIntent = new Intent(getApplicationContext(),RajshahiActivity.class);
+                startActivity(dcIntent);
+            }
+        });
+        button2 = (Button) findViewById(R.id.khulnaButton);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent dcIntent = new Intent(getApplicationContext(),RajshahiActivity.class);
+                startActivity(dcIntent);
+            }
+        });
+        button2 = (Button) findViewById(R.id.bariButton);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent dcIntent = new Intent(getApplicationContext(),RajshahiActivity.class);
+                startActivity(dcIntent);
+            }
+        });
+        button2 = (Button) findViewById(R.id.sylButton);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent dcIntent = new Intent(getApplicationContext(),RajshahiActivity.class);
+                startActivity(dcIntent);
+            }
+        });
+        button2 = (Button) findViewById(R.id.myButton);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent dcIntent = new Intent(getApplicationContext(),RajshahiActivity.class);
+                startActivity(dcIntent);
+            }
+        });
+    }
+}

b1d2cb220ecb80f7eca736825d73850e850b7be2
Mayisa Sanchita <mayisasanchita@gmail.com>
2019-03-20 12:49:29 +0600

    MedicineInf activity is added

---
+++ app/src/main/java/com/example/medicare/MedicineInfActivity.java
@@ -0,0 +1,26 @@
+package com.example.medicare;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MedicineInfActivity extends AppCompatActivity {
+    private Button button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medicine_inf);
+        button2 = (Button) findViewById(R.id.myButton);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent dcIntent = new Intent(getApplicationContext(),MymensinghActivity.class);
+                startActivity(dcIntent);
+            }
+        });
+    }
+}
